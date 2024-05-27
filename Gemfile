07cbf3e8e2b7ec98ca49e2333a0e3fb1e6d4f207
Kadina1988 <kadina.aziev@mail.ru>
2024-05-27 09:47:33 +0300

    added active storagee validations gem

--- Gemfile
+++ Gemfile
@@ -67,3 +67,4 @@ end
 
 gem 'omniauth-github', '~> 2.0.0'
 gem "omniauth-rails_csrf_protection"
+gem 'active_storage_validations'
--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -5,23 +5,11 @@ class Bulletin < ApplicationRecord
   validates :description, presence: true, length: { maximum: 1000 }
   validates :image, presence: true
   validates :image, presence: true
-  validates :image, attached: true, content_type: 'image/png'
-  # validate :image_validation
+  validates :image, attached: true, content_type: [:png, :jpg, :jpeg],
+                                    size: { less_than: 5.megabytes, message: 'is too large' }
 
   belongs_to :user
   belongs_to :category
 
   scope :first_new, -> { order(created_at: :desc) }
-
-  # def image_validation
-  #   if image.attached?
-  #     if image.blob.byte_size > 5242880
-  #       image.purge
-  #       errors[:base] >> 'Image too big'
-  #     elsif !image.blob.content_type.starts_with?('image/')
-  #       image.purge
-  #       errors[:base] << 'Wrong format'
-  #     end
-  #   end
-  # end
 end
