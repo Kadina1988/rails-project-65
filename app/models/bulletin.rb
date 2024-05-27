dc80e38c84d52f86c0eb1b0e7706ee6f3522d98b
Kadina1988 <kadina.aziev@mail.ru>
2024-05-27 09:41:39 +0300

    bullet

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -3,9 +3,25 @@ class Bulletin < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 1000 }
+  validates :image, presence: true
+  validates :image, presence: true
+  validates :image, attached: true, content_type: 'image/png'
+  # validate :image_validation
 
   belongs_to :user
   belongs_to :category
 
   scope :first_new, -> { order(created_at: :desc) }
+
+  # def image_validation
+  #   if image.attached?
+  #     if image.blob.byte_size > 5242880
+  #       image.purge
+  #       errors[:base] >> 'Image too big'
+  #     elsif !image.blob.content_type.starts_with?('image/')
+  #       image.purge
+  #       errors[:base] << 'Wrong format'
+  #     end
+  #   end
+  # end
 end
